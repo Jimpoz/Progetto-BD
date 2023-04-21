ce44f19f4c6244e3e77f07fc0763e74ad0cc9749
Jinpeng Zhang <79638739+Jimpoz@users.noreply.github.com>
2023-04-21 15:17:30 +0200

    Test di flask con 4 route (non funziona non c'è app)

--- app.py
+++ app.py
@@ -17,9 +17,47 @@
 import flask
 from flask import *;
 from flask_login import *
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!' #per adesso da cambiare
+app.secret_key = 'supersecretkey'
 
-login_manager = LoginManager()
-login_manager.init_app(app)
+# Pagina di benvenuto
+@app.route('/')
+def welcome():
+    return render_template('welcome.html')
+
+# Login
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Verifica le credenziali
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == 'admin':
+            # Login valido, crea la sessione
+            session['username'] = username
+            return redirect(url_for('dashboard'))
+        else:
+            # Login fallito, mostra un messaggio di errore
+            return render_template('login.html', error='Credenziali non valide')
+    else:
+        # Mostra la pagina di login
+        return render_template('login.html')
+
+# Area riservata
+@app.route('/dashboard')
+def dashboard():
+    if 'username' in session:
+        # Utente autenticato, mostra il pannello di controllo
+        return render_template('dashboard.html', username=session['username'])
+    else:
+        # Utente non autenticato, reindirizza alla pagina di login
+        return redirect(url_for('login'))
+
+# Logout
+@app.route('/logout')
+def logout():
+    # Rimuovi la sessione e reindirizza alla pagina di login
+    session.pop('username', None)
+    return redirect(url_for('login'))
