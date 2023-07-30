a8e5ddf189cc99fac737397cf1a19c0cbfa8407f
andreramolivaz <92636448+andreramolivaz@users.noreply.github.com>
2023-07-30 19:46:30 +0200

    Utilizzato libreria os per le variabili d'ambiente. D'ora in poi per accedere alla secret key e al db si passa per il file `.env` che di norma non dovrebbe essere committato ma ve lo lascio cosi non dovete ricrearvelo :)

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,24 @@ import os
 from flask import Flask
 from flask_migrate import Migrate
 from routes import bp as routes_bp
+##carico le variabili d'ambiente contenute nel file .env
+from dotenv import load_dotenv
+load_dotenv()
 
 def create_app():
     from routes import login_manager
     from db_setup import db
+
     app = Flask(__name__)
     login_manager.init_app(app)
     login_manager.login_view = 'routes.login'
     #app.config['SQLALCHEMY_DATABASE_URI'] =\
     #    'sqlite:///' + os.path.join(app.root_path, 'database.db')
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:secret_key@localhost:5433/Progetto_BD'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
-    app.config['SECRET_KEY'] = 'secret_key'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
     migrate = Migrate(app, db)
 
