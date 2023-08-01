bcb36b2196d41a8c78d3401fcbfb6a85d5d38c0e
Jinpeng Zhang <jimpo.jpz@gmail.com>
2023-08-01 22:21:06 +0200

    semplificato file app.py

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 from app import create_app
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.debug = True
-    app.config['DEBUG'] = True
     app.run()
